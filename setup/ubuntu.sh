0d9e0be7e7ca3d1c27c64c3edbfa72efbff36755
Vincent Caboara <vcaboara@gmail.com>
2016-08-04 13:31:14 -0700

    ubuntu.sh - neovim, chrome, docker-engine/compose

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-APPS_LIST="vim zsh screen workrave \
+APPS_LIST="neovim screen workrave chromium-browser \
 	git openssh-server python-pip build-essential cmake \
 	python-dev exfat-fuse exfat-utils iotop apt-transport-https \
-	ca-certificates virtualbox"
+	ca-certificates virtualbox docker-engine"
+
 PYTHON_MODULES="argparse"
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> \
-	/etc/apt/sources.list.d/docker.list
+# add neovim ppa
+add-apt-repository -y ppa:neovim-ppa/unstable
 
 # Update your local package index:
 apt-get update # To get the latest package lists
 apt-get upgrade -y
 apt-get install -y $APPS_LIST
 
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 python -m pip install $PYTHON_MODULES
 
 # Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install YCM
 #cd ~/.vim/bundle/YouCompleteMe
